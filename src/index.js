f3b749d70de26dbbfbe5ab301e465ca3d6bb8743
Adekunle Ismail <adismail4@gmail.com>
2022-01-22 12:28:15 +0100

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -9,21 +9,16 @@ const todoList = [
 const list = document.querySelector('.list');
 const renderTodos = () => {
   list.innerHTML = '';
-  todoList.forEach((todo) => {
+  todoList.forEach((t) => {
     list.innerHTML += `
-      <li id="task-${todo.index}" draggable="true">
+      <li id="task-${t.index}" draggable="true">
         <div class="content">
-          <input class="check" type="checkbox" ${
-            todo.completed ? 'checked' : ''
-          }/>
-          <input class="input" type="text" value='${
-            todo.description
-          }' readonly />
+          <input class="check" type="checkbox" ${t.completed ? 'checked' : ''}/>
+          <input class="input" type="text" value='${t.description}' readonly />
         </div>
         <div class="actions">
          <i class="fas fa-trash-alt delete hide"></i>
-         <i class="fas fa-ellipsis-v more-todo"></i>
-                
+         <i class="fas fa-ellipsis-v more-todo"></i>                
         </div>
       </li>
       `;
