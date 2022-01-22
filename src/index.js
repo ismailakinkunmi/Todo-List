41a5ed4a62502e0f35126043c5c9fb184097dc5e
Adekunle Ismail <adismail4@gmail.com>
2022-01-22 13:31:57 +0100

    Modify index.js, index.html, style.css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import './style.css';
+import Todos from './todos.js';
 
-const todoList = [
-  { description: 'meeting', completed: true, index: 1 },
-  { description: 'coding', completed: false, index: 2 },
-  { description: 'watching movie', completed: true, index: 3 },
-];
+const todos = new Todos();
 
 const list = document.querySelector('.list');
 const renderTodos = () => {
   list.innerHTML = '';
-  todoList.forEach((t) => {
+  todos.list.forEach((t) => {
     list.innerHTML += `
-      <li id="task-${t.index}" draggable="true">
+      <li id="${t.index}" draggable="true">
         <div class="content">
           <input class="check" type="checkbox" ${t.completed ? 'checked' : ''}/>
           <input class="input" type="text" value='${t.description}' readonly />
@@ -23,5 +20,43 @@ const renderTodos = () => {
       </li>
       `;
   });
+
+  document.querySelectorAll('.input').forEach((inp) => {
+    inp.addEventListener('change', () => {
+      const task = todos.list[+inp.parentNode.parentNode.id - 1];
+      task.description = inp.value;
+      todos.update(task);
+      renderTodos();
+    });
+  });
+
+  document.querySelectorAll('.delete').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      todos.remove(+btn.parentNode.parentNode.id);
+      renderTodos();
+    });
+  });
+
+  document.querySelectorAll('.more-todo').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      btn.classList.add('hide');
+      const parent = btn.parentNode.parentNode;
+      parent.querySelector('.delete').classList.remove('hide');
+      parent.classList.add('active');
+      const input = parent.querySelector('.input');
+      input.readOnly = false;
+      input.focus();
+    });
+  });
 };
 renderTodos();
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  todos.add({
+    description: form.elements.inputfield.value,
+  });
+  renderTodos();
+  form.reset();
+});
